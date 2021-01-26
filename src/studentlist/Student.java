e9e84938701849dff3da0df4ad41fc9744f3a4ed
bask01 <bask@sheridancollege.ca>
2021-01-25 20:19:16 -0500

    Added an address field

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package studentlist;
 
 /**
@@ -12,10 +7,22 @@ package studentlist;
 public class Student {
     
     private String name;
-
+    
+    private String address;
+    
     public Student(String name) {
         this.name = name;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
     
     public String getName() {
         return name;
