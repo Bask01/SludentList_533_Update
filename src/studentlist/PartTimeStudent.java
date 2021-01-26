413ba41340caee3abe7a7529ab0667b580e31521
bask01 <bask@sheridancollege.ca>
2021-01-25 21:45:00 -0500

    Added part-time student

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,27 @@
+package studentlist;
+
+/**
+ *
+ * @author kbas0
+ */
+public class PartTimeStudent extends Student{
+    private int numCourses;
+
+    public PartTimeStudent(int numCourses, String name) {
+        super(name);
+        this.numCourses = numCourses;
+    }
+
+    public int getNumCourses() {
+        return numCourses;
+    }
+
+    public void setNumCourses(int numCourses) {
+        this.numCourses = numCourses;
+    }
+
+  
+
+ 
+    
+}
